6a86fd2675596cb6e0bfde8ebcf271129b9ac8b8
HamzaAliKundi <hamza.alee83@gmail.com>
2025-07-26 22:56:03 +0500

    add contact api and design

--- src/store.tsx
+++ src/store.tsx
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './apis/auth';
 import { userApi } from './apis/user/users';
+import { contactApi }from "./apis/user/users/contact"
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
-    [userApi.reducerPath]: userApi.reducer
+    [userApi.reducerPath]: userApi.reducer,
+    [contactApi.reducerPath]: contactApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware).concat(userApi.middleware)
+    getDefaultMiddleware().concat(authApi.middleware).concat(userApi.middleware).concat(contactApi.middleware)
 });
