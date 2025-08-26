8920b2baaa72df2537fbd957adb672ea565a675c
Fahad <fahadjamil521@gmail.com>
2025-08-26 22:16:31 +0500

    dropdown fixed

--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -158,7 +158,7 @@ const PaymentForm = ({
                 <option value="">Select tag color</option>
                 <option value="blue">Blue</option>
                 <option value="red">Red</option>
-                <option value="pink">Pink</option>
+                <option value="yellow">Yellow</option>
               </select>
             </div>
 
